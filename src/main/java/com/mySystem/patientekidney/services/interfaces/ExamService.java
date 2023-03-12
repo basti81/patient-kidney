c5e6aa36184f399d32432ebc3dc2976bccf733e4
basti81 <bastian.g.astudillo.r@gmail.com>
2023-03-11 19:38:44 -0600

    Version a arreglar

---
+++ src/main/java/com/mySystem/patientekidney/services/interfaces/ExamService.java
@@ -0,0 +1,16 @@
+package com.mySystem.patientekidney.services.interfaces;
+
+
+import com.mySystem.patientekidney.models.entities.Exam;
+import com.mySystem.patientekidney.models.entities.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExamService {
+    List<Exam> findAllExam();
+    Exam saveExam (Exam patient);
+    Optional<Exam> getExamById(Long idExam);
+    void deleteExamById(Long idExam);
+    List<Exam> saveAllExam(List<Exam> patients);
+}
